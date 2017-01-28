b0936cb2b55bbe668bd7c226ed231d8dc8932521
kornikkk <kornik1992@gmail.com>
2017-01-28 17:00:12 +0100

    Added javadoc for utils

--- src/pl/kkbo/matrices/matrix/MatrixUtils.scala
+++ src/pl/kkbo/matrices/matrix/MatrixUtils.scala
@@ -7,6 +7,14 @@ import pl.kkbo.matrices.matrix.exceptions.MatrixOperationException
   */
 object MatrixUtils {
 
+  /**
+    * Allows to add two matrices of the same size
+    *
+    * @param matrix      matrix to which other matrix will be added
+    * @param otherMatrix matrix to add
+    * @throws pl.kkbo.matrices.matrix.exceptions.MatrixOperationException if size of matrices is different
+    * @return result matrix
+    */
   @throws(classOf[MatrixOperationException])
   def add(matrix: Matrix, otherMatrix: Matrix): Matrix = {
     if (matrix.height != otherMatrix.height || matrix.width != otherMatrix.width)
@@ -20,6 +28,14 @@ object MatrixUtils {
     result
   }
 
+  /**
+    * Allows to subtract two matrices of the same size
+    *
+    * @param matrix      matrix from which other matrix will be subtracted
+    * @param otherMatrix matrix to subtract
+    * @throws pl.kkbo.matrices.matrix.exceptions.MatrixOperationException if size of matrices is different
+    * @return result matrix
+    */
   @throws(classOf[MatrixOperationException])
   def subtract(matrix: Matrix, otherMatrix: Matrix): Matrix = {
     if (matrix.height != otherMatrix.height || matrix.width != otherMatrix.width)
@@ -32,4 +48,6 @@ object MatrixUtils {
 
     result
   }
+
+
 }

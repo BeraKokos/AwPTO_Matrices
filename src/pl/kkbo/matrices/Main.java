3719f0cae8fa57fe6eadf75ca6d6c7bcf11f1328
kornikkk <kornik1992@gmail.com>
2017-01-28 13:28:42 +0100

    Initial commit

---
+++ src/pl/kkbo/matrices/Main.java
@@ -0,0 +1,7 @@
+package pl.kkbo.matrices;
+
+/**
+ * Created by korni on 28.01.2017.
+ */
+public class Main {
+}
---
+++ src/pl/kkbo/matrices/matrix/Matrix.java
@@ -0,0 +1,7 @@
+package pl.kkbo.matrices.matrix;
+
+/**
+ * Created by kkarolcz on 28.01.2017.
+ */
+public class Matrix {
+}
